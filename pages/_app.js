7a1f8f233a9d28dff629e64f24ffab060f2ca160
Jule <bingthemachine@gmail.com>
2023-02-05 22:10:54 +0100

    inicial setup with hygraph integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import React, { useState, useEffect } from "react";
+import { Layout } from "../components";
+import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
---
+++ services/index.js
@@ -0,0 +1,92 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+        postsConnection {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+`
+  const result = await request(graphqlAPI, query)
+  return result.postsConnection.edges;
+}
+
+export const getRecentPosts = async () => {
+  const query = gql`
+  query GetPostDetails() {
+    posts(orderBy: createdAt_ASC
+      last: 4
+    ){
+      title
+      featuredImage{
+        url
+      }
+      createdAt
+      slug
+  }
+}
+  `
+  const result = await request(graphqlAPI, query)
+  return result.posts;
+}
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories{
+      categories{
+        name 
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+}; 
